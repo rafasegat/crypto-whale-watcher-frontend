93d440fc3d5350986d277519cf47f0e362f6a53e
Rafael Segat <rafasegat@gmail.com>
2021-07-23 16:57:32 +1000

    feature: firstrelease

--- src/rest/endpoints.tsx
+++ src/rest/endpoints.tsx
@@ -2,7 +2,9 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://crypto-whale-watcher-prod.herokuapp.com/";
 
-export const getTransactions = async (symbol: string, done: Function) => {
-  const { data } = await axios.get(`/transactions?symbol=${symbol}`);
+export const getTransactions = async (filter: TypeFilter, done: Function) => {
+  const { data } = await axios.get(
+    `/transactions?symbol=${filter.symbol}&period${filter.period}`
+  );
   done(data.results);
 };
